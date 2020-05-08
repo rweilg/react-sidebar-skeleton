2b71cbfb529410ac3231d97822884dbeeb84f33f
Rodrigo Weilg <contact@rodrigoweilg.com>
2020-05-08 15:15:07 +0200

    base

---
+++ src/canvas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./canvas.css";
+
+function Canvas() {
+  return (
+    <div className="whole-page">
+      <div className="frame">
+        <h1>canvas</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Canvas;

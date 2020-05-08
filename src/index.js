41d62b001cd44f78c921c7c07a476ca60564467c
Rodrigo Weilg <contact@rodrigoweilg.com>
2020-05-09 00:14:33 +0200

    removed references

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import App from "./app";
 
 ReactDOM.render(
@@ -10,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
